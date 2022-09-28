a0c495c729e19c9e0ecb50f327946e0f5290563b
Liunardy <liunardyliunardy@gmail.com>
2022-09-28 10:01:35 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('ISEC6000 W9'));
+app.get('/', (req, res) => res.send('ISEC6000 W9: Pipeline Test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
